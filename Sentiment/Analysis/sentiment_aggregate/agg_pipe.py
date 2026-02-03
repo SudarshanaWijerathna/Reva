16e0baa06c8cb8565394e3753fd5bea3032b758e
kavishks3942 <63503856+kavishka3942@users.noreply.github.com>
2026-02-03 20:23:40 +0530

    Sentiment aggregator

--- Sentiment/Analysis/sentiment_aggregate/agg_pipe.py
+++ Sentiment/Analysis/sentiment_aggregate/agg_pipe.py
@@ -1,6 +1,8 @@
 from Analysis.sentiment_aggregate.aggregator import MarketSentimentAggregator
+from Analysis.sentiment_aggregate.state_cache import AggregatorService
 from Analysis.storage.store import SentimentStorage
 
+#Aggregator without Cache
 repo = SentimentStorage()
 docs = repo.fetch_relevant_docs()
 print(f"Fetched {len(docs)} relevant documents for aggregation.")
@@ -9,3 +11,16 @@ aggregator = MarketSentimentAggregator(docs)
 market_sentiment = aggregator.aggregate()
 
 print(market_sentiment)
+
+
+# there is a prblame with a catche implementation in state_cache.py. Individually other 2 piplines works perfectly
+
+#Aggregator with Cache
+'''repo = SentimentStorage()
+docs = repo.fetch_relevant_docs()
+print(f"Fetched {len(docs)} relevant documents for aggregation.")
+
+service = AggregatorService(ttl_seconds=300)
+market_sentiment = service.get_market_state(docs)
+
+print(market_sentiment)'''

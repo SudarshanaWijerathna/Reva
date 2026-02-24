80759afa899b4a7caa3176c90e6447ad690e0e4a
kavishks3942 <63503856+kavishka3942@users.noreply.github.com>
2026-02-24 21:48:33 +0530

    scheduler update

--- backend/core/scheduler.py
+++ backend/core/scheduler.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta, timezone
 from apscheduler.schedulers.background import BackgroundScheduler
 from backend.core.cache_service import update_sentiment_cache
 from Sentiment.Analysis.run_both import run_both_pipelines
@@ -6,6 +7,6 @@ from Sentiment.Analysis.run_both import run_both_pipelines
 scheduler = BackgroundScheduler()
 
 def start_scheduler():
-    scheduler.add_job(update_sentiment_cache, "interval", minutes=5)
-    scheduler.add_job(run_both_pipelines, "interval", days=7)
+    scheduler.add_job(update_sentiment_cache, "interval", minutes=15, next_run_time=datetime.now(timezone.utc) + timedelta(minutes=15))  # Update cache every 5 minutes
+    scheduler.add_job(run_both_pipelines, "interval", days=7, next_run_time=datetime.now(timezone.utc) + timedelta(days=7))
     scheduler.start()
